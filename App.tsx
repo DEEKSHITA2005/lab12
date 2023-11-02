af25e9bf6c4c8b85ff453c6622801ef5bc9d9abe
DEEKSHITA2005 <108971466+DEEKSHITA2005@users.noreply.github.com>
2023-11-02 20:45:18 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//import MuiTypoGraphy from './Components/MuiTypoGraphy';
+import MuiButton from './Components/MuiButton';
+import MuiTextField from './Components/MuiTextField';
+import MuiCard from './Components/MuiCard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MuiTypoGraphy/ > */}
+      {/* <MuiButton/> */}
+      {/* <MuiTextField/> */}
+      <MuiCard/>
+    </div>
+  );
+}
+
+export default App;
